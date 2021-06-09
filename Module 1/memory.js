3c7a309bdb95623ebba7dd4ea335f1355e9e8d5e
Sejal <sejalaggarwal0@gmail.com>
2021-06-09 18:41:44 +0530

    submit

---
+++ Module 1/memory.js
@@ -0,0 +1,27 @@
+console.log(multiply);
+
+function multiply(a,b){
+    return a*b;
+}
+
+console.log(add);
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(x);
+
+var x = multiply(10,10);
+
+console.log(y);
+
+var y = add(x , 20);
+
+console.log(x);
+console.log(y);
+
+// Hoisting =>
+
+console.log(a);
+var a = 20;
