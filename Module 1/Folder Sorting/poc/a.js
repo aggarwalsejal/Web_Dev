cfb0a9cdda7f2e7262c2ffc44c22854bd8f682c2
Sejal <sejalaggarwal0@gmail.com>
2021-06-14 13:48:18 +0530

    submit

---
+++ Module 1/Folder Sorting/poc/a.js
@@ -0,0 +1,4 @@
+let importedData = require("./b.js");
+
+
+importedData() 
---
+++ Module 1/Folder Sorting/poc/b.js
@@ -0,0 +1,13 @@
+let a = 10;
+
+// module.exports = {}
+
+// module.exports.a = a;
+// module.exports.name = "hello";
+
+module.exports = a;
+module.exports = "Steve";
+
+module.exports = function () {
+  console.log("i came from b");
+};
